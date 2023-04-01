1ac014eb59c81f6b5bc4a575e933974ce7177662
carraes <carraeshb@gmail.com>
2023-04-01 15:22:55 -0300

    refactor: a to Link, since it wont use outside links anymore

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -2,6 +2,7 @@ import type { RouterOutputs } from '~/utils/api'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import Image from 'next/image'
+import Link from 'next/link'
 
 dayjs.extend(relativeTime)
 
@@ -13,20 +14,24 @@ const PostView = ({ post, author }: PostWithUser) => {
 
   return (
     <div key={id} className="flex gap-4 border-b border-slate-400 p-8">
-      <Image
-        src={profileImageUrl}
-        alt="Profile image"
-        className="w-14 h-14 rounded-full"
-        width={56}
-        height={56}
-      />
+      <Link href={`/@${username}`}>
+        <Image
+          src={profileImageUrl}
+          alt="Profile image"
+          className="w-14 h-14 rounded-full"
+          width={56}
+          height={56}
+        />
+      </Link>
       <div className="flex flex-col gap-2">
         <div className="flex gap-2 text-sm text-slate-300 self-center">
-          <a href={`https://github.com/${username}`}>
+          <Link href={`/@${username}`}>
             <span className="font-bold hover:underline duration-200">{`@${username}`}</span>
-          </a>
-          <span>·</span>
-          <span>{`${dayjs(createdAt).fromNow()}`}</span>
+          </Link>
+          <Link href={`/post/${id}`} className="flex gap-2 hover:underline duration-200">
+            <span>·</span>
+            <span className="text-xs self-center">{`${dayjs(createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-white">{content}</span>
       </div>
