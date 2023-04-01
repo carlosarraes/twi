909f99c40705765f698c69de2a929145033a0251
carraes <carraeshb@gmail.com>
2023-04-01 11:06:33 -0300

    feat: add functionality to post if authorized

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,8 @@
 import type { User } from '@clerk/nextjs/dist/api'
 import { clerkClient } from '@clerk/nextjs/server'
 import { TRPCError } from '@trpc/server'
-import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
+import { createTRPCRouter, publicProcedure, privateProcedure } from '~/server/api/trpc'
+import { z } from 'zod'
 
 const filterUserForClient = (user: User) => {
   return {
@@ -15,6 +16,11 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
     })
 
     const users = (
@@ -39,4 +45,22 @@ export const postsRouter = createTRPCRouter({
       }
     })
   }),
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().min(1).max(280),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      })
+
+      return post
+    }),
 })
