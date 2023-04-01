05bdf215a8894f45792a11689f36b319ba9682b5
carraes <carraeshb@gmail.com>
2023-04-01 11:22:23 -0300

    feat: setup upstash to rate limit 3 posts per minute

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,6 +3,8 @@ import { clerkClient } from '@clerk/nextjs/server'
 import { TRPCError } from '@trpc/server'
 import { createTRPCRouter, publicProcedure, privateProcedure } from '~/server/api/trpc'
 import { z } from 'zod'
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
 
 const filterUserForClient = (user: User) => {
   return {
@@ -11,6 +13,12 @@ const filterUserForClient = (user: User) => {
     profileImageUrl: user.profileImageUrl,
   }
 }
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, '1 m'),
+  analytics: true,
+})
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -53,6 +61,15 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId
+
+      const { success } = await ratelimit.limit(authorId)
+
+      if (!success) {
+        throw new TRPCError({
+          code: 'TOO_MANY_REQUESTS',
+          message: 'You are posting too fast',
+        })
+      }
 
       const post = await ctx.prisma.post.create({
         data: {
