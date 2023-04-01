86a00d0af01948e6843f77fb472aba02d615b8d0
carraes <carraeshb@gmail.com>
2023-04-01 09:08:28 -0300

    feat: feed component, so it separates the user/posts loading

---
+++ src/components/Feed.tsx
@@ -0,0 +1,21 @@
+import { api } from '~/utils/api'
+import PostView from './PostView'
+import Spinner from './Spinner'
+
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery()
+
+  if (postsLoading) return <Spinner size={40} />
+
+  if (!data) return <section>Something went wrong</section>
+
+  return (
+    <section>
+      {data?.map(({ post, author }) => (
+        <PostView key={post.id} post={post} author={author} />
+      ))}
+    </section>
+  )
+}
+
+export default Feed
