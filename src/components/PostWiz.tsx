9ec967bb9e13e02e2455ffae038ae0528ada1ed7
carraes <carraeshb@gmail.com>
2023-04-01 11:05:30 -0300

    feat: Post tweets connection, update state once succeed

--- src/components/PostWiz.tsx
+++ src/components/PostWiz.tsx
@@ -1,28 +1,43 @@
 import { useUser } from '@clerk/nextjs'
-import Image from 'next/image'
+import { useState } from 'react'
+import { api } from '~/utils/api'
+import { BsFillSendFill } from 'react-icons/bs'
 
 const PostWiz = () => {
   const { user } = useUser()
+  const [input, setInput] = useState('')
+
+  const ctx = api.useContext()
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput('')
+      void ctx.posts.getAll.invalidate()
+    },
+  })
 
   if (!user) {
     return <div>Not signed in</div>
   }
 
   return (
-    <>
+    <section className="flex gap-4 p-2">
       <input
         type="text"
         placeholder="What's happening?"
         className="bg-slate-800 text-white rounded-full w-full h-10 pl-4 outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="w-14 h-14 rounded-full"
-        width={56}
-        height={56}
-      />
-    </>
+      <button
+        type="button"
+        className="flex w-10 justify-center items-center bg-slate-800 text-white rounded-full outline-none"
+        onClick={() => mutate({ content: input })}
+      >
+        <BsFillSendFill className="self-center" />
+      </button>
+    </section>
   )
 }
 
